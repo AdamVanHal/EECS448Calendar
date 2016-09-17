941976eaa5495690d7356e5f99e3051255d60d59
Daniel Murga <danmur14@gmail.com>
2016-09-17 16:16:52 -0500

    Add files via upload

--- calendar_provider.php
+++ calendar_provider.php
@@ -1,32 +1,54 @@
-<?
-//set up sql connection to eecs server
+<?php
 
-$method = $_REQUEST['method'];
-$server = "mysql.eecs.ku.edu";
-$username = "dmurga";
-$db = new mysqli($server, $username, "Ns5qKknK", "dmurga");
+class Calendar {
 
-switch $method {
-	case "getEvents":
+	public function getEvents() {
+		$db = $this->getDB();
 		$records = $db->query("SELECT * FROM calendar");
 		$data = array();
 		while($row = $records->fetch_assoc()) {
 			$data[] = $row; 
 		}
 		return $data;
-		break;
-	case "addEvent":
-		$insert_string = $_REQUEST['name'] . ", " . $_REQUEST['description'] . ", " . $_REQUEST['event_date'] . ", " . "0";
+	}
+	public function addEvent($name, $description, $event_date) {
+		$db = $this->getDB();
+		$insert_string = "'" . $name . "', '" . $description . "', '" . $event_date . "', " . "0";
 		$db->query("INSERT INTO calendar (name, description, event_date, user) VALUES (" . $insert_string . ")");
-		break;
-	case "deleteEvent":
-		$db->query("DELETE FROM calendar WHERE event_id=" . $_REQUEST['event_id']);
-		break;
-	/*
-	case "editEvent":
-		$db->query("UPDATE SET " . )
-		break;
-	*/
+	}
+	public function deleteEvent($event_id) {
+		$db = $this->getDB();
+		$db->query("DELETE FROM calendar WHERE event_id=" . $event_id);
+	}
+	public function getDB() {
+		$server = "mysql.eecs.ku.edu";
+		$username = "dmurga";
+		$db = new mysqli($server, $username, "Ns5qKknK", $username);
+		return $db;
+	}
+	// public function editEvent($name, $description, $event_date) {
+	// }
 }
+$calendar = new Calendar;
+
+$method = $_REQUEST['method'];
+if($method) {
+	switch ($method) {
+		case "getEvents":
+			$data = $calendar->getEvents();
+			break;
+		case "addEvent":
+			$calendar->addEvent($_REQUEST['name'], $_REQUEST['description'], $_REQUEST['event_date']);
+			break;
+		case "deleteEvent":
+			$calendar->deleteEvent($_REQUEST['event_id']);
+			break;
+		// case "editEvent":
+		// 	$calendar->editEvent($_REQUEST['name'], $_REQUEST['description'], $_REQUEST['event_date']);
+		// 	break;
+		
+	}
+}
+
 
 ?>
